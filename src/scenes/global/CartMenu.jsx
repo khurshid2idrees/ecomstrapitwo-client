01e508bc82027239ce4e85286bc6504fd65930d5
Khurshid2idrees <umarkhurshid3@gmail.com>
2024-02-01 18:41:14 +0530

    update in all components

---
+++ src/scenes/global/CartMenu.jsx
@@ -0,0 +1,37 @@
+import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import styled from "@emotion/styled";
+import { shades } from "../../theme";
+import {
+  decreaseCount,
+  increaseCount,
+  addToCart,
+  removeFromCart,
+  setIsCartOpen,
+} from "../../state";
+
+import { useNavigate } from "react-router-dom";
+
+const FlexBox = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CartMenu = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+  const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+
+  const totalPrice = cart.reduce((total, item) => {
+    return total + item.count * item.attributes.price;
+  }, 0);
+
+  return <Box display={isCartOpen ? "block" : "none"}></Box>;
+};
+
+export default CartMenu;
